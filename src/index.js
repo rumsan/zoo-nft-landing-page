21c1789a960f34fcd77439df81a656f147ea1a86
Raktim Shrestha <raktim@rumsan.com>
2022-03-30 00:30:41 +0545

    pre push

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import $ from 'jquery';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'), function () {
+ReactDOM.render(<App />, document.getElementById('root'), () => {
 	$('#preloader').delay(0).fadeOut();
 });
